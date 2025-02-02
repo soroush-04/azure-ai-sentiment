fb616e9c8159a4491cc293eb538fe0bdaa7f4953
Soroush Kami <soroushkami10@gmail.com>
2025-02-02 14:07:11 -0500

    Clean up

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import feedback_view, feedback_api, play_response, download_audio
+from .views import feedback_api, play_response, download_audio
 
 urlpatterns = [
-    path('', feedback_view, name='feedback_form'),
     path("submit-feedback/", feedback_api, name="submit-feedback"),
     path("play-response/", play_response, name="play-response"),
     path('download-audio/', download_audio, name='download_audio'),
--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -1,33 +1,11 @@
 import json
 import os
-from django.shortcuts import render
 from django.http import JsonResponse, FileResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from .forms import FeedbackForm
 from .azure_text_analytics import analyze_sentiment
 from .azure_speech import text_to_speech
 from .response_generator import generate_response
 from django.conf import settings
-
-
-def feedback_view(request):
-    if request.method == "POST":
-        form = FeedbackForm(request.POST)
-        if form.is_valid():
-            feedback_text = form.cleaned_data['feedback']
-            sentiment = analyze_sentiment(feedback_text)
-            text_to_speech(feedback_text)
-            response_text = generate_response(feedback_text)
-            
-            return render(request, 'feedback/feedback_form.html', {
-                'form': form, 
-                'sentiment': sentiment,
-                'response_text': response_text
-            })
-    else:
-        form = FeedbackForm()
-
-    return render(request, 'feedback/feedback_form.html', {'form': form})
 
 @csrf_exempt  # disable for testing
 def feedback_api(request):
